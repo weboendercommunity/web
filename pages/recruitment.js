c63f391783c81fabce36131814b7b9dab2a38953
Revaldi Rahmatmulya <revaldiaye@gmail.com>
2021-09-26 11:44:01 +0700

    fix button loading

--- pages/recruitment.js
+++ pages/recruitment.js
@@ -1,11 +1,9 @@
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbwLqnWhyue9c-V_gIuwRDDDXL73yICXw49XpfassWe1rOCbrJWR1Mv_BXX9oA7WPYdQ/exec";
-const form = document.forms["google-sheet"];
-const btnKirim = document.querySelector(".btn-kirim");
-const btnLoading = document.querySelector(".btn-loading");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+const storeToGsheets = () => {
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbwLqnWhyue9c-V_gIuwRDDDXL73yICXw49XpfassWe1rOCbrJWR1Mv_BXX9oA7WPYdQ/exec";
+  const form = document.forms["google-sheet"];
+  const btnKirim = document.querySelector(".btn-kirim");
+  const btnLoading = document.querySelector(".btn-loading");
 
   btnLoading.classList.toggle("d-none");
   btnKirim.classList.toggle("d-none");
@@ -15,4 +13,32 @@ form.addEventListener("submit", (e) => {
     btnKirim.classList.toggle("d-none");
     location.reload(true);
   });
-});
+};
+
+// script validation
+
+(function () {
+  "use strict";
+
+  var forms = document.querySelectorAll(".needs-validation");
+
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+        if (form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          storeToGsheets();
+        }
+      },
+      false
+    );
+  });
+})();
